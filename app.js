4174315d007cc08c591ad904d46d6d814f852751
julienlegoux <lgx.julien@gmail.com>
2022-09-08 19:57:53 +0200

    8.9.2022

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express')
-const {success} = require('./helper')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
+const bodyParser = require('body-parser')
+const {success, getUniqueId} = require('./helper')
 let pokemons = require('./mock-pokemons')
 
 const app = express()
 const port = 3000
 
-app.get('/', (req,res) => res.send('hello, Express 2 !'))
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(bodyParser.json())
+
+app.get('/', (req,res) => res.send('hello, Express !'))
 
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
@@ -19,4 +27,21 @@ app.get('/api/pokemons', (req, res) => {
     res.json(success(message, pokemons))
 })
 
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons)
+    const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
+    pokemons.push(pokemonCreated)
+    const message = `Le pokemon ${pokemonCreated.name} a bien été créé.`
+    res.json(success(message, pokemonCreated))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = { ...req.body, id: id}
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+})
 app.listen(port, () => console.log(`Notre app est ouverte sur http://localhost:${port}`))
