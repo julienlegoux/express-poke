9761124e26bd6e373f9dde4edae687024da19515
julienlegoux <lgx.julien@gmail.com>
2022-09-09 20:08:15 +0200

    9.9.2022 Bdd sequelize

--- app.js
+++ app.js
@@ -2,11 +2,36 @@ const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
 const bodyParser = require('body-parser')
+const {Sequelize, DataTypes} = require('sequelize')
 const {success, getUniqueId} = require('./helper')
 let pokemons = require('./mock-pokemons')
+const PokemonModel = require('./src/models/pokemon')
 
 const app = express()
 const port = 3000
+
+const sequelize = new Sequelize(
+    'pokedex', // name bdd
+    'root', // username bdd
+    '', // password bdd
+    {
+        host: 'localhost', // mandatory
+        dialect: 'mariadb', // mandatory
+        dialectOptions: {
+            timezone: 'Etc/GMT-2'
+        },
+        logging: false
+    }
+)
+
+sequelize.authenticate()
+    .then(_ => console.log('La connexion est établie avec la bdd'))
+    .catch(error => console.error(`Impossible de se connecter à la bdd ${error}`))
+
+const Pokemon = PokemonModel(sequelize,DataTypes)
+
+sequelize.sync({force:true})
+    .then(_ => console.log('La bdd Pokedex a bien été sync!'))
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
@@ -14,6 +39,7 @@ app
     .use(bodyParser.json())
 
 app.get('/', (req,res) => res.send('hello, Express !'))
+
 
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
@@ -44,4 +70,13 @@ app.put('/api/pokemons/:id', (req, res) => {
     const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
     res.json(success(message, pokemonUpdated))
 })
+
+app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDeleted.name} a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
+})
+
 app.listen(port, () => console.log(`Notre app est ouverte sur http://localhost:${port}`))
